55ad0b7588650ba61fba65ca4d6b71008d95e4e7
= <84853932+PmanProject@users.noreply.github.com>
2024-05-31 23:46:55 +0000

    more csp stuff

--- csp.js
+++ csp.js
@@ -11,32 +11,9 @@ class PVar {
         this.id = player.id;
         this.name = player.name;
         this.power = player.power;
-        this.whitelist = new Set();
-        if (player.whitelist.length) {
-            this.setWhitelist(player.whitelist);
-        }
-        this.blacklist = new Set();
-        if (player.blacklist.length) {
-            this.setBlacklist(player.blacklist);
-        }
         
         this.value = null;
         this.domain = new Set();
-    }
-    /* Sets the whitelist variable as the set or iterable */
-    setWhitelist(whitelist) {
-        if (typeof whitelist !== 'Set') {
-            whitelist = new Set(whitelist);
-        }
-        this.whitelist = whitelist;
-    }
-
-    /* Sets the blacklist variable as the set or iterable */
-    setBlacklist(blacklist) {
-        if (typeof blacklist !== 'Set') {
-            blacklist = new Set(blacklist);
-        }
-        this.blacklist = blacklist;
     }
 
     /* Adds the domain to the given variable */
@@ -74,14 +51,6 @@ class Constraint {
     }
 }
 
-class Heuristics {
-    constructor () {}
-
-    selectVariable() {
-
-    }
-
-}
 
 class CSPModel {
     constructor() {
@@ -89,22 +58,101 @@ class CSPModel {
         this.constraints = new Set();
     }
     
-    addVariable(player) {
+    addVariable(pVar) {
+        this.variables.add(pVar);
+    }
+
+    addConstraint(constraint) {
+        this.constraints.add(constraint);
+    }
+
+}
+
+function basicPropagator(CSP, nextVar) {
+    let prunings = new Set();
+    let constraints = CSP.constraints;
+    let allSatisfied = false;
+    constraints.every((constr) => {
+        if (constr.isSatisfied()) {
+            return true;
+        }
+        allSatisfied = false;
+        return false;
+    });
+
+    return allSatisfied, prunings;
+}
+
+class Solver {
+    constructor(CSP) {
+        this.CSP = CSP;
+        this.assignments = new Set();
+    }
+
+    nextVariable() {
+        // TODO: heuristic approach based on domain sizes?
+        // TODO: additional sort based on play numbers here
+        var returnVar = null;
+        // This could be more efficient if a set could be iterated through easier...
+        // right now its changing arrays from sets
+        this.variables.every((varib) => {
+            if (!varib.value) {
+                returnVar = varib;
+                return false;
+            }
+            return true;
+        });
+        return returnVar;
+    }
+
+    backtrackSolve(assignments) {
+        let shouldContinue, prunings;
+        
+        let nextVar = this.nextVariable();
+        shouldContinue, prunings = basicPropagator(this, nextVar);
+        if (shouldContinue && nextVar !== null) {
+            for(var i = 0; i < nextVar.domain.size; i++) {
+                let nextValue = nextVar.domain[i];
+                nextVar.assingValue(nextValue);
+
+                // prune other players after power level
+            }
+        }
 
     }
 
     solve() {
+        this.backtrackSolve([]);
+    }
+
+    getSolution() {
 
     }
 }
+
 // ########### ??? idno what to name this, other stuff that uses CSP stuff but is not directly CSP stuff ##########
 
 class Player {
-    constructor(id, name, power, whitelist, blacklist) {
+    constructor(id, name, power) {
         this.id = id;
         this.name = name;
         this.power = power;
+        this.whitelist = new Set();
+        this.blacklist = new Set();
+    }
+    /* Sets the whitelist variable as the set or iterable */
+    setWhitelist(whitelist) {
+        if (typeof whitelist !== 'Set') {
+            whitelist = new Set(whitelist);
+        }
         this.whitelist = whitelist;
+    }
+
+    /* Sets the blacklist variable as the set or iterable */
+    setBlacklist(blacklist) {
+        if (typeof blacklist !== 'Set') {
+            blacklist = new Set(blacklist);
+        }
         this.blacklist = blacklist;
     }
 }
@@ -124,26 +172,85 @@ function collectPlayers() {
         let id = elem.id;
         id = id.split("-")[1];
         let name = elem.querySelector('player-name').innerHTML.trim();
+        let newP = new Player(id, name, power, whitelist, blacklist);
         // TODO: add whitelist
-        let whitelist = new Set();
+        //newP.setWhitelist(stuff);
         // TODO: add blacklist
-        let blacklist = new Set();
+        //newP.setBlacklist(stuff);
+        playersList.push(newP);
+    });
+}
+
+function addConstraints(CSP, playersList) {
+    /*
+    let powerlevelSets = {
+        1: new Set(), // casual
+        2: new Set(), // medium
+        3: new Set(), // high
+        4: new Set()  // comp
+    };
+    */
+    
+    let blacklists = new Set();
+    let whitelists = new Set();
+    
+    playersList.forEach((p1) => {
         
-        playersList.push(
-            new Player(id, name, power, whitelist, blacklist)
+        // powerlevelSets[p1.power].add(p1);
+
+        p1.blacklists.forEach((p2) => {
+            blacklists.add(new Set([p1.id, p2.id]))
+        });
+        
+        p1.whitelist.forEach((p2) => {
+            whitelists.add(new Set([p1.id, p2.id]))
+        });
+    });
+    // Add blacklist constraints
+    blacklists.forEach((subset) => {
+        let p1 = subset[0];
+        let p2 = subset[1];
+        CSP.addConstraint(
+            new Constraint("blacklist-" + p1.id + "-" + p2.id, () => {return p1.value !== p2.value;})
+        );
+    });
+    // Add whitelist constraints
+    whitelists.forEach((subset) => {
+        let p1 = subset[0];
+        let p2 = subset[1];
+        CSP.addConstraint(
+            new Constraint("whitelist-" + p1.id + "-" + p2.id, () => {return p1.value === p2.value;})
         );
     });
 }
 
-/* Models the */
+/* Models the problem as a CSP */
 function createCSPProblem() {
     let CSP = new CSPModel();
 
     let players = collectPlayers();
+    let maxDomainInt = Math.round(players.length / 2);
     players.forEach((player) => {
         CSP.addVariable(player);
+        CSP.setDomain(
+            Set.from(
+                Array(maxDomainInt).keys()
+            )
+        );
     });
+    addConstraints(CSP, players);
+    return CSP;
+}
 
-    let blacklists = new Set();
-    let whitelsits = new Set();
+/*
+    basically the main function
+    collects all info from the DOM and performs the search
+    returns an array of arrays, with each sub-array containing ids of the pairings
+    e.x. [[1, 12, 50, 57], [2, 3, 4, 5], ...]
+*/
+function sortPlayers() {
+    let CSP = createCSPProblem();
+    let solver = new Solver(CSP);
+    solver.solve();
+    solver.getSolution();
 }
