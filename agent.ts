f0c051a91a18dfd2e81490a4a174804117d800bb
= <84853932+PmanProject@users.noreply.github.com>
2024-06-03 13:10:42 +0000

    heuristic stuff and patching

--- agent.ts
+++ agent.ts
@@ -3,19 +3,126 @@ const MAXSEATS = 4;
 var PLAYERS:Record<number, Player> = {};
 // TODO: async
 
+class MinHeap {
+    heap:Array<State>;
+    h:Heuristic
+    constructor(h:Heuristic) {
+        this.h = h;
+        this.heap = [];
+    }
+ 
+    // Helper Methods
+    getLeftChildIndex(parentIndex:number) {
+        return 2 * parentIndex + 1;
+    }
+    getRightChildIndex(parentIndex:number) {
+        return 2 * parentIndex + 2;
+    }
+    getParentIndex(childIndex:number) {
+        return Math.floor((childIndex - 1) / 2);
+    }
+    hasLeftChild(index:number) {
+        return this.getLeftChildIndex(index) < this.heap.length;
+    }
+    hasRightChild(index:number) {
+        return this.getRightChildIndex(index) < this.heap.length;
+    }
+    hasParent(index:number) {
+        return this.getParentIndex(index) >= 0;
+    }
+    leftChild(index:number) {
+        return this.heap[this.getLeftChildIndex(index)];
+    }
+    rightChild(index:number) {
+        return this.heap[this.getRightChildIndex(index)];
+    }
+    parent(index:number) {
+        return this.heap[this.getParentIndex(index)];
+    }
+ 
+    // Functions to create Min Heap
+     
+    swap(indexOne:number, indexTwo:number) {
+        const temp = this.heap[indexOne];
+        this.heap[indexOne] = this.heap[indexTwo];
+        this.heap[indexTwo] = temp;
+    }
+ 
+    peek() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        return this.heap[0];
+    }
+     
+    // Removing an element will remove the
+    // top element with highest priority then
+    // heapifyDown will be called 
+    remove() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        const item = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        this.heapifyDown();
+        return item;
+    }
+ 
+    add(item:State) {
+        this.heap.push(item);
+        this.heapifyUp();
+    }
+ 
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
+            this.swap(this.getParentIndex(index), index);
+            index = this.getParentIndex(index);
+        }
+    }
+ 
+    heapifyDown() {
+        let index = 0;
+        while (this.hasLeftChild(index)) {
+            let smallerChildIndex = this.getLeftChildIndex(index);
+            if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
+                smallerChildIndex = this.getRightChildIndex(index);
+            }
+            if (this.heap[index] < this.heap[smallerChildIndex]) {
+                break;
+            } else {
+                this.swap(index, smallerChildIndex);
+            }
+            index = smallerChildIndex;
+        }
+    }
+     
+    printHeap() {
+        var heap =` ${this.heap[0]} `
+        for(var i = 1; i<this.heap.length;i++) {
+            heap += ` ${this.heap[i]} `;
+        }
+        console.log(heap);
+    }
+}
 
 class Heuristic {
     /*
         Score numbers (High = bad)
     */
-    HIGHERPOWER:number = 25;    // Cost for each higher power that exist if theres a lower power in the pod
-    WHITELIST:number = 0;       // Cost for Whitelist players are at same table
-    BLACKLIST:number = 100;     // Cost for Blacklisted players are at same table
-    EMPTYSEAT:number = 10;      // Cost for each player who hasnt been seated
+    POWERDIFF:number = 25;          // Cost for level higher power that exist if theres a lower power in the pod
+                                    // Each level higher adds a squared multiplier to this score (1 level diff = 2x, 2 levels diff = 4x)
+    WHITELIST:number = 0;           // Cost for Whitelist players are at same table
+    BLACKLIST:number = 100;         // Cost for Blacklisted players are at same table
+    EMPTYSEAT:number = 10;          // Cost for each player who hasnt been seated (default cost)
 
     maxPlayers:number;
-
-    constructor(maxplayers:number) {this.maxPlayers = maxplayers;}
+    hashmap:Record<number, number>; // hash: value
+    constructor(maxplayers:number) {
+        this.maxPlayers = maxplayers;
+        this.hashmap = {};
+    }
 
     evalPlayer(playersSet:Set<number>) : Player|null {
         // TODO: pick player with estimated least branching paths
@@ -39,44 +146,70 @@ class Heuristic {
     }
 
     evalState(state:State) {
+        let stateHash = state.hash();
+        if ((stateHash) in this.hashmap) {
+            return this.hashmap[stateHash];
+        }
         // TODO: return "heuristic score" of the state
         var totalScore = 0;
         var totalSeated = state.totalSeatedPlayers();
+        var player:Player;
         // add score for total unseated players
         totalScore += (this.maxPlayers - totalSeated) * this.EMPTYSEAT;
         // search for black- & whitelist + differing powerlevels
         for (var table of state.tables) {
             var tableScore = 0;
-            var seatsByPower = {};
-            var blackListSet = new Set();
-            var whiteListSet = new Set();
+            var seatsByPower:Record<number, Set<number>> = {};
+            var totalDifferingPowers = 0;
+            
             for (let objPlayerId of table.seats) {
-                var player:Player = PLAYERS[objPlayerId[0]];
-                addValToDictSet(seatsByPower, player.power, player);
+                player = PLAYERS[objPlayerId[0]];
+                totalDifferingPowers += addValToDictSet(seatsByPower, player.power, player.id);
                 if (player.hasBlacklist()) {
                     if(table.containsBlackList(player)) {
-                        blackListSet.add(player);
+                        // add whitelist score
+                        tableScore += this.BLACKLIST;
                     }
                 }
                 if (player.hasBlacklist()) {
                     if(table.containsWhitelist(player)) {
-                        whiteListSet.add(player);
+                        // add blacklist score
+                        tableScore += this.WHITELIST;
                     }
                 }
             }
-            // add blacklist score
-            tableScore += blackListSet.size * this.BLACKLIST;
-            // add whitelist score
-            tableScore += whiteListSet.size * this.WHITELIST;
+            // check powerlevels
+            if (totalDifferingPowers > 1) {
+                var differingPowers = 0;
+                var powerLevel:string, playerSet:Set<number>;
+
+                for ([powerLevel, playerSet] of Object.entries(seatsByPower)) {
+                    // more higher power than lower are preferrable over more lower over higher
+                    // for each player at higher, increment by one 
+                    let PL:number = Number(powerLevel);
+                    if (String(PL + 1) in seatsByPower) {
+                        differingPowers += playerSet.size * 1;
+                    }
+                    if (String(PL + 2) in seatsByPower) {
+                        differingPowers += playerSet.size * 2;
+                    }
+                    if (String(PL + 3) in seatsByPower) {
+                        differingPowers += playerSet.size * 4;
+                    }
+                }
+                tableScore += differingPowers * this.POWERDIFF;
+            }
+            totalScore += tableScore;
         }
+        return totalScore;
     }
     
 }
 
 class Environment {
-    playersList:Array<Player>
-    maxTables:number
-    tables:Set<Table>
+    playersList:Array<Player>;
+    maxTables:number;
+    tables:Array<Table>;
 
     constructor(players:Array<Player>) {
         this.maxTables = Math.round(players.length / 3);
@@ -93,7 +226,7 @@ class Environment {
 
     constructTables() {
         for (let i = 0; i < this.maxTables; i++) {
-            this.tables.add(
+            this.tables.push(
                 new Table(i + 1)
             );
         }
@@ -106,35 +239,68 @@ class Environment {
 
     legalActions(state:State, player:Player) : Array<Table> {
         // legal seats for player instead of (for each player, for each table) decreases the scope by a considerable margin
-        var availableTables:Array<Table> = []
+        var availableTables:Array<Table> = [];
         for (let table of state.tables) {
             if (!table.isFull()) {
-                if (!table.containsBlackList(player)) {
-                    availableTables.push(table);
-                }
+                availableTables.push(table);
             }
         }
         return availableTables;
     }
 
     getInitialState() : State {
-        return new State(null, this.tables);
+        let playersIdSet:Set<number> = new Set();
+        Array.prototype.forEach((player) => {playersIdSet.add(player.id)}, this.playersList);
+        return new State(null, this.tables, playersIdSet);
     }
 }
 
 class State {
-    prevState:State|null
-    tables:Array<Table>
+    prevState:State|null;
+    tables:Array<Table>;
+    playersLeft:Set<number>;
     // a possible assignment of variables of course
-    constructor(prevState:State|null, tables:Set<Table>) {
+    constructor(prevState:State|null, tables:Array<Table>, playersLeft:Set<number>|Array<number>) {
         this.prevState = prevState;
-        this.tables = Array.from(tables);
+        this.tables = [];
+        this.playersLeft = new Set(playersLeft);
+        Array.prototype.forEach((table) => {this.tables.push(table.copy())}, tables);
+    }
+
+    copy() : State {
+        return new State(this.prevState, this.tables, this.playersLeft);
     }
     totalSeatedPlayers() {
         return this.tables.reduce((prev, curr) => {return prev + curr.seatedPlayers()}, 0);
     }
-    hash() {
-        // do I need this?
+    nextState(playerId:number, toTable:number) : State {
+        let stateCpy = this.copy();
+        stateCpy.tables[toTable].seatPlayer(playerId);
+        return stateCpy;
+    }
+    hash() : number {
+        // do I need this?.. yes
+        // OC (https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript)
+        // donut steel
+
+        // basically change the whole id list of seats to strings and hash the strings
+        var str = "";
+        let table:Table;
+        for (table of this.tables) {
+            // sets shouldnt change order, so ordering doesnt matter 
+            for (let player of table.seats.entries()) {
+                str += String(player[0]) + "-";
+            }
+        }
+        var hash = 0,
+            i:number, chr:number;
+        if (str.length === 0) return hash;
+        for (i = 0; i < str.length; i++) {
+            chr = str.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
     }
 
 }
@@ -157,15 +323,17 @@ class SearchAgent {
 
         var bestAction:number|null = null;
         var bestScore:number = Infinity;
-        
+        var frontier:Array<State> = [];
         // choose which player to order first
         var player:Player|null = this.heuristic.evalPlayer(this.unsortedPlayers);
         // when player has been chosen, choose which table to sit him at
-        let actions = this.env.legalActions(currentState, player);
-        for (let action in actions) {
-            let score = this.heuristic.evalState(player, action);
-            if (score == bestScore) {
-                bestAction = action
+        if (player !== null) {
+            let actions = this.env.legalActions(previousState, player);
+            for (let action in actions) {
+                let score = this.heuristic.evalState(player, action);
+                if (score == bestScore) {
+                    bestAction = action
+                }
             }
         }
         // pick best value first, recursively check deeper until all are assigned
@@ -188,6 +356,17 @@ class Table {
     constructor(id:number) {
         this.id = id;
         this.seats = new Set();
+    }
+
+    copy() {
+        let newTbl = new Table(this.id);
+        this.seats.forEach(seat => {
+            newTbl.seats.add(seat);
+        });
+    }
+
+    seatPlayer(playerId:number) {
+        this.seats.add(playerId);
     }
     
     isFull() {
@@ -279,12 +458,18 @@ function collectPlayers():Array<Player> {
     return playersList;
 }
 // HELPER FUNCTIONS
-/* Adds a value to a dict's value set, if key does not exist, create a new set */
-function addValToDictSet(dictObj:Record<number, Set<object>>, key:number, value:object) {
+/*
+    Adds a value to a dict's value set, if key does not exist, create a new set
+    Returns 1 if a new set was created within the record 
+*/
+function addValToDictSet(dictObj:Record<number, Set<number>>, key:number, value:number): number {
+    var added = 0;
     if (!(key in dictObj)) {
         dictObj[key] = new Set();
+        added ++;
     }
     dictObj[key].add(value);
+    return added;
 }
 // MAIN FUNCTION
 function doSearch() {
