8e7dcbc090bf9c6495fdc8fe1545ce4355691f6b
Pallrp <prp@palli.org>
2024-06-03 17:24:03 +0000

    adding players to playerlist

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 function loadPage() : undefined {
+    addPowerSelections();
+    addButtonEvents();
     addPlayer("Maximus Timmy", Powerlevel.CASUAL);
     addPlayer("Bruhman Lower", Powerlevel.MEDIUM);
     addPlayer("John Doe", Powerlevel.MEDIUM);
@@ -6,6 +8,48 @@ function loadPage() : undefined {
     addPlayer("Chad.", Powerlevel.COMP);
 }
 
+function clearForm(formId:string) {
+    let form = document.getElementById(formId);
+    if (form !== null) {
+        form.querySelectorAll('input').forEach((target) => {
+            target.value = "";
+        });
+        form.querySelectorAll('select').forEach((target) => {
+            target.querySelectorAll('option').forEach((optTarget) => {
+                optTarget.selected = false;
+            });
+        });
+    }
+}
+
+function addButtonEvents() {
+    let addPlayerButton = document.getElementById('submit-player');
+    if (addPlayerButton !== null) {
+        addPlayerButton.addEventListener("click", (ev) => {
+            let playerName = (document.getElementById('add-player-name') as HTMLInputElement).value;
+            let playerPower = (document.getElementById('add-player-power') as HTMLOptionElement).value;
+            console.log(playerName + "-" + playerPower);
+            if (playerName && playerPower) {
+
+                addPlayer(playerName, Number(playerPower));
+                clearForm('add-player-form');
+            }
+        });
+    }
+}
+
+function addPowerSelections() {
+    let powerSelect = document.getElementById("add-player-power");
+    if (powerSelect !== null) {
+        for (const [key, value] of Object.entries(Powerlevel)) {
+            let powerName:string = getPowerVerboseName(value);
+            let optionEl:HTMLElement = document.createElement('option');
+            optionEl.setAttribute('value', String(value));
+            optionEl.innerHTML = powerName;
+            powerSelect.appendChild(optionEl);
+        }
+    }
+}
 const playerTemplateEl = (document.getElementById('player-container-template') as HTMLElement);
 const playerContainer = (document.getElementById('players-container') as HTMLElement);
 var playerCount = 0;
